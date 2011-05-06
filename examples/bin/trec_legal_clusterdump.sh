35f39e1f0382244a48b7f9bee643ad0cec7bf3d7
peterld <peter@helioid.com>
2011-05-06 06:49:16 -0400

    loop over all topics

--- examples/bin/trec_legal_clusterdump.sh
+++ examples/bin/trec_legal_clusterdump.sh
@@ -2,18 +2,6 @@
 
 for topic in 200 201 202 203 204 205 206 207
 do
-  # clear old data
-  rm -r out-trec_legal_$topic-kmeans/*
-
-  bin/mahout kmeans       --input            out-trec_legal_$topic-vectors/tfidf-vectors \
-                          --output           out-trec_legal_$topic-kmeans/clusters \
-                          --clusters         out-trec_legal_$topic-kmeans/initialclusters \
-                          --maxIter          10 \
-                          --numClusters      5 \
-                          --clustering       \
-                          --overwrite
-  wait
-
   bin/mahout clusterdump  --seqFileDir        out-trec_legal_$topic-kmeans/clusters/clusters-1 \
                           --pointsDir         out-trec_legal_$topic-kmeans/clusters/clusteredPoints \
                           --numWords          50 \
--- examples/bin/trec_legal_vectors.sh
+++ examples/bin/trec_legal_vectors.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-bin/mahout seqdirectory --input             ../data/terse/enron \
-                        --output            out-trec_legal-seqfiles \
-                        --charset           utf-8
+for topic in 200 201 202 203 204 205 206 207
+do
+  # clear old data
+  rm -r out-trec_legal_$topic-seqfiles
+  rm -r out-trec_legal_$topic-vectors
 
-wait
+  bin/mahout seqdirectory --input             ../data/rel/$topic/1 \
+                          --output            out-trec_legal_$topic-seqfiles \
+                          --charset           utf-8
 
-bin/mahout seq2sparse   --input             out-trec_legal-seqfiles    \
-                        --output            out-trec_legal-vectors     \
-                        --maxNGramSize      2                        \
-                        --namedVector                                \
-                        --minDF             4                        \
-                        --maxDFPercent      75                       \
-                        --weight            TFIDF                    \
-                        --norm              2                        \
-                        --analyzerName      org.apache.mahout.analysis.TRECLegalAnalyzer
+  wait
+
+  bin/mahout seq2sparse   --input             out-trec_legal_$topic-seqfiles    \
+                          --output            out-trec_legal_$topic-vectors     \
+                          --maxNGramSize      2                        \
+                          --namedVector                                \
+                          --minDF             4                        \
+                          --maxDFPercent      75                       \
+                          --weight            TFIDF                    \
+                          --norm              2                        \
+                          --analyzerName      org.apache.mahout.analysis.TRECLegalAnalyzer
+done
 
